d905b20e66cc61f5c4e44e3ece5df58553e230d7
Conor Egan <68134729+c-eg@users.noreply.github.com>
2021-05-15 15:37:48 +0100

    init models

---
+++ cogs/models/game.py
@@ -0,0 +1,5 @@
+class Game(object):
+    def __init__(self, max_players=6):
+        self.max_players = max_players
+        self.team_one = []
+        self.team_two = []
---
+++ cogs/models/server.py
@@ -0,0 +1,4 @@
+class Server(object):
+    def __init__(self):
+        self.queue = []
+        self.games = []
