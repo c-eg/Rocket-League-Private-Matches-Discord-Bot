3802162804d3ca28698ca83f08d6a7e45f874d3f
Conor Egan <68134729+c-eg@users.noreply.github.com>
2021-05-15 15:01:28 +0100

    cog support

--- main.py
+++ main.py
@@ -1,20 +1,19 @@
 import discord
+from discord.ext import commands
 from dotenv import load_dotenv, find_dotenv
 import os
 
-load_dotenv(find_dotenv())
-client = discord.Client()
+load_dotenv(find_dotenv()) # load .env file
+client = commands.Bot(command_prefix = ';')
 
 @client.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    print('Bot successfully started!')
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+# Load cogs into client
+for file in os.listdir('./cogs'):
+    if file.endswith('.py'):
+        client.load_extension(f'cogs.{file[:-3]}')
+        print(f'Loaded {file}')
 
 client.run(os.environ.get('BOT_TOKEN'))
