e770d8ccdbb09d812b0906ef775617e750d77ea6
Conor Egan <68134729+c-eg@users.noreply.github.com>
2021-11-16 18:37:03 +0000

    captains should work now

--- models/game_captains.py
+++ models/game_captains.py
@@ -2,6 +2,7 @@
 
 from discord.ext import commands
 from asyncio import TimeoutError
+import time
 
 from models.game import Game
 from models.no_player_action_exception import NoPlayerActionException
@@ -14,14 +15,14 @@ class CaptainsGame(Game):
         self.bot = bot
 
     async def assign_teams(self):
+        """
+        Assigns teams based on the two captains choices.
+        """
         captain_a = self.players.pop(0)
-        # captain_b = self.players.pop(0)
+        captain_b = self.players.pop(0)
 
-        print(f"CAP A: {captain_a}")
-        # print(f"CAP B: {captain_b}")
-
-        captain_a_message = "Please pick a player to be on your team, type the number corresponding to the player e.g. 2\n\n"
-
+        # CAPTAIN A FIRST PICK
+        captain_a_message = "You have 90 seconds to pick.\nPlease pick a player to be on your team, type the number corresponding to the player e.g. 2\n\n"
         counter = 1
 
         for player in self.players:
@@ -30,22 +31,85 @@ class CaptainsGame(Game):
 
         disc_message = await captain_a.get_discord_user().send(captain_a_message)
 
-        try:
-            reply = await self.bot.wait_for(event='message', timeout=60, check=lambda message: message.channel == disc_message.channel)
-        except TimeoutError:
-            raise NoPlayerActionException()
+        listen_for_captain_a = True
+        time_start = time.time() + 90
 
-        reply = int(reply)
+        while not listen_for_captain_a:
+            try:
+                reply = await self.bot.wait_for(event='message', timeout=time_start - time.time(), check=lambda message: message.channel == disc_message.channel)
+                reply = int(reply)
 
-        """
-        TODO:
-        - Get bot to send a message to user if they don't send a number in the correct range.
-        - Then remove that user from the queue, add to team 1
-        - Then let captain 2 choose two players
-        - Then assign last player to team 1
-        """
+                if 1 <= reply <= 4:
+                    listen_for_captain_a = False
+                    await captain_a.get_discord_user().send(f"You picked: {self.players[reply - 1].get_discord_user().mention}")
+                else:
+                    await captain_a.get_discord_user().send("Please enter a number between 1 and 4!")
+            except TimeoutError:
+                listen_for_captain_a = False
+                raise NoPlayerActionException()
 
-        # if not 1 <= reply <= len(self.players):
+        self.team_one.push(self.players.pop(reply - 1))
 
+        # CAPTAIN B FIRST PICK
+        captain_b_message = "You have 2 minutes to pick.\nPlease pick a player to be on your team, type the number corresponding to the player e.g. 2\n\n"
+        counter = 1
 
-        # print(reply.content)
+        for player in self.players:
+            captain_a_message += f"{counter}) {player.get_discord_user().mention}\n"
+            counter += 1
+
+        disc_message = await captain_b.get_discord_user().send(captain_b_message)
+
+        listen_for_captain_b = True
+        time_start = time.time() + 120
+
+        while listen_for_captain_b:
+            try:
+                reply = await self.bot.wait_for(event='message', timeout=time_start - time.time(), check=lambda message: message.channel == disc_message.channel)
+                reply = int(reply)
+
+                if 1 <= reply <= 3:
+                    listen_for_captain_a = False
+                    await captain_a.get_discord_user().send(f"You picked: {self.players[reply - 1].get_discord_user().mention}")
+                else:
+                    await captain_a.get_discord_user().send("Please enter a number between 1 and 3!")
+            except TimeoutError:
+                listen_for_captain_a = False
+                raise NoPlayerActionException()
+
+        self.team_two.push(self.players.pop(reply - 1))
+
+        # CAPTAIN B SECOND PICK
+        captain_b_message = "Please pick another player to be on your team, type the number corresponding to the player e.g. 2\n\n"
+        counter = 1
+
+        for player in self.players:
+            captain_a_message += f"{counter}) {player.get_discord_user().mention}\n"
+            counter += 1
+
+        disc_message = await captain_b.get_discord_user().send(captain_b_message)
+
+        listen_for_captain_b = True
+
+        while listen_for_captain_b:
+            try:
+                reply = await self.bot.wait_for(event='message', timeout=time_start - time.time(), check=lambda message: message.channel == disc_message.channel)
+                reply = int(reply)
+
+                if 1 <= reply <= 2:
+                    listen_for_captain_a = False
+                    await captain_a.get_discord_user().send(f"You picked: {self.players[reply - 1].get_discord_user().mention}")
+                else:
+                    await captain_a.get_discord_user().send("Please enter the number 1 or 2!")
+            except TimeoutError:
+                listen_for_captain_a = False
+                raise NoPlayerActionException()
+
+        self.team_two.push(self.players.pop(reply - 1))
+
+        # ASSIGN CAPTAIN A (Team 1) LAST PLAYER
+        self.team_one.push(self.players.pop(0))
+
+        # ASSIGN CAPTAIN A TO TEAM 1 AND CAPTAIN B TO TEAM 2
+        self.team_one.insert(0, captain_a)
+        self.team_two.insert(0, captain_b)
