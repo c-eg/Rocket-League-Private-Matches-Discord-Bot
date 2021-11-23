49f9345c834d34ca69eed1b8a89fc60d43765b95
Araf Ahmed <araf0216@gmail.com>
2021-11-22 20:14:25 -0500

    Embed class updated to sub of discord.Embed

--- static/embed_template.py
+++ static/embed_template.py
@@ -4,12 +4,8 @@ import discord
 
 class EmbedTemplate(discord.Embed):
     def __init__(self, title, colour):
-        self.super(EmbedTemplate, self).__init__()
-        self.embed = discord.Embed(
-            title=title,
-            colour=colour,
-        )
-        self.embed.set_footer(
+        super().__init__(title, colour)
+        self.set_footer(
             text="UEA Private Matches by curpha",
             icon_url="https://cdn.akamai.steamstatic.com/steamcommunity/public/images/avatars/be/bed810f8bebd7be235b8f7176e3870de1006a6e5_full.jpg",
         )
