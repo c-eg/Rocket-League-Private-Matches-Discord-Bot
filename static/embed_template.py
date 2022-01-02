95a0adbbb66c420d2618c588b8fa459347bae446
Conor Egan <68134729+c-eg@users.noreply.github.com>
2022-01-02 05:45:37 +0000

    Slight refactor

--- static/embed_template.py
+++ static/embed_template.py
@@ -5,6 +5,7 @@ import discord
 class EmbedTemplate(discord.Embed):
     def __init__(self, title, colour):
         super().__init__(title=title, colour=colour)
+
         self.set_footer(
             text="UEA Private Matches by curpha",
             icon_url="https://cdn.akamai.steamstatic.com/steamcommunity/public/images/avatars/be/bed810f8bebd7be235b8f7176e3870de1006a6e5_full.jpg",
